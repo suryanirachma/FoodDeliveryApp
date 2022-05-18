25fd84b6fbe19c37328da07ea56f170fe254f456
Suryani Rachma <suryanirachma@webmail.umm.ac.id>
2022-05-18 13:34:37 +0700

    mutation

--- FoodService/Graphql/Mutation.cs
+++ FoodService/Graphql/Mutation.cs
@@ -25,42 +25,43 @@ namespace FoodService.Graphql
 
             return ret.Entity;
         }
-    }
 
-    //[Authorize(Roles = new[] { "Admin", "Manager" })]
-    public async Task<Food> UpdateFoodAsync(
+        //[Authorize(Roles = new[] {"Manager" })]
+        public async Task<Food> UpdateFoodAsync(
             FoodInput input,
             [Service] FoodDeliveryContext context)
-    {
-        var food = context.Foods.Where(o => o.Id == input.Id).FirstOrDefault();
-        if (food != null)
         {
-            food.Name = input.Name;
-            food.Stock = input.Stock;
-            food.Price = input.Price;
+            var food = context.Foods.Where(o => o.Id == input.Id).FirstOrDefault();
+            if (food != null)
+            {
+                food.Name = input.Name;
+                food.Stock = input.Stock;
+                food.Price = input.Price;
 
-            context.Foods.Update(food);
-            await context.SaveChangesAsync();
+                context.Foods.Update(food);
+                await context.SaveChangesAsync();
+            }
+
+
+            return await Task.FromResult(food);
         }
 
-
-        return await Task.FromResult(food);
-    }
-
-    //[Authorize(Roles = new[] { "Manager" })]
-    public async Task<Food> DeleteFoodByIdAsync(
-        int id,
-        [Service] FoodDeliveryContext context)
-    {
-        var food = context.Foods.Where(o => o.Id == id).FirstOrDefault();
-        if (food != null)
+        //[Authorize(Roles = new[] { "MANAGER" })]
+        public async Task<Food> DeleteFoodByIdAsync(
+            int id,
+            [Service] FoodDeliveryContext context)
         {
-            context.Foods.Remove(food);
-            await context.SaveChangesAsync();
+            var food = context.Foods.Where(o => o.Id == id).FirstOrDefault();
+            if (food != null)
+            {
+                context.Foods.Remove(food);
+                await context.SaveChangesAsync();
+            }
+
+
+            return await Task.FromResult(food);
         }
 
-
-        return await Task.FromResult(food);
     }
 
 }
